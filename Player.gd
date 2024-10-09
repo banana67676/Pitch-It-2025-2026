f1b5db9f46478cef4a6877b580284ffa7d07257d
xnossisx <xnossisx@gmail.com>
2024-10-08 19:10:08 -0500

    Initial multiplayer setup

---
+++ Player.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	if is_multiplayer_authority():
+		global_position = get_global_mouse_position()
