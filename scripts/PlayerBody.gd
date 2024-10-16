356193a378a76a44f60bf01c563c38b3e31b9361
santa <teagan@itsys.pro>
2024-10-16 18:22:17 -0500

    organized folders

--- scripts/PlayerBody.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
-	if is_multiplayer_authority():
-		global_position = get_global_mouse_position()
