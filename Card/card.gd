3860adf740ba0a4473e2b819be0aa5863b4d60f1
santa <teagan@itsys.pro>
2024-10-16 18:46:30 -0500

    re added folders to commit

---
+++ Card/card.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@export var FLIP_TIME = .5
+
+@onready var front: Node2D = $Front
+@onready var back: Node2D = $Back
+
+@onready var currentSide = back
+
+enum {
+	stay,
+	shrinking,
+	growing
+}
+var state = stay
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func shrink() -> Tween:
+	print("shrinking")
+	var shrinkTween = create_tween()
+	shrinkTween.tween_property(self, "scale:x", 0, FLIP_TIME)
+	currentSide.visible = false
+	print("done shrinking")
+	return shrinkTween
+
+func grow() -> Tween:
+	print("growing")
+	currentSide.visible = true
+	var growTween = create_tween()
+	growTween.tween_property(self, "scale:x", 1, FLIP_TIME)
+	print("done growing?")
+	return growTween
+
+func swap_current_side():
+	if currentSide == back:
+		currentSide = front
+	elif currentSide == front:
+		currentSide = back
+	else:
+		print("side is null")
+
+func flip() -> void:
+	state = shrinking
+	var shrinkTween = shrink()
+	await shrinkTween.finished
+	swap_current_side()
+	state = growing
+	var growTween = grow()
+	await growTween.finished
+	print("actually done growing")
+	state = stay
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		print("card clicked")
+		flip()
---
+++ Scenes/Multiplayer Menu/Player.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var drawing_scene: PackedScene
+var drawing
+func _start():
+	preload("../scenes/Multiplayer_Menu.tscn")
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+	drawing = drawing_scene.instantiate()
+	get_parent().call_deferred("add_child", drawing)
