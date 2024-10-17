91dc8d309a611c8d39a70cb1fcfcc2af2bef8049
Teagan Thomes <teagan@anydesk.net>
2024-10-17 08:34:50 -0500

    added more folders

--- Scenes/Multiplayer Menu/Player.gd
+++ Scenes/Multiplayer Menu/Player.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var drawing_scene: PackedScene
 var drawing
 func _start():
-	preload("../scenes/Multiplayer_Menu.tscn")
+	preload("./Multiplayer_Menu.tscn")
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
