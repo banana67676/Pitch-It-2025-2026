b5282b4687053a8b1f92c35634d4c8982323d180
XnossisX <xnossisx@gmail.com>
2024-11-01 12:18:21 -0500

    Added transition to card creation state

---
+++ pitch_card_data.gd
@@ -0,0 +1,14 @@
+class PitchCardData:
+	var title
+	var slogan
+	var user
+	var logo
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
