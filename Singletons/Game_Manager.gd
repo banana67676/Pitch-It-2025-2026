741e1beffacb0cf393b96eec504a44b44ee77d96
Arched2135 <3029452@SPARK-iMac-11.parkwayschools.net>
2024-11-06 12:05:22 -0600

    State and settings

--- Singletons/Game_Manager.gd
+++ Singletons/Game_Manager.gd
@@ -1,6 +1,17 @@
 extends Node
 
 
-enum game_state {
-	test
+enum game_state_enum{
+	title,
+	main_menu,
+	lobby,
+	creation,
+	presentaion,
+	voting,
+	results,
+	end_game
 }
+
+var game_state : int = game_state_enum.title
+
+var settings : bool = false
