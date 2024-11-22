9e7542c60f2fe825c114d06e4dd8c9ace88486f6
arched2135 <3029452@pkwyesxi01.parkwayschools.net>
2024-11-22 12:14:12 -0600

    creation to display fix

--- Card/Card Content/card.gd
+++ Card/Card Content/card.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 enum card_types {
 	who,
@@ -108,11 +108,11 @@ enum card_types {
 
 var possible_text = []
 
-@export var text: String
 
 @onready var front = $Front
 @onready var back = $Back
-@onready var label: Label = $Front/MarginContainer/ColorRect/MarginContainer/Label
+@onready var label: Label = $Face/MarginContainer/ColorRect/MarginContainer/Label
+
 
 @onready var currentSide = back
 
@@ -125,6 +125,7 @@ var state = stay
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(1).timeout
 	if (card_type == card_types.who):
 		possible_text = who_text
 	elif (card_type == card_types.what):
@@ -134,62 +135,12 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("ui_accept")):
-		print("card clicked")
-		flip()
-		#test()
+func _physics_process(delta: float) -> void:
+	move(Vector2(0,0))
 
 func pick_text():
-	if (text == ""):
-		text = possible_text[randi_range(0, possible_text.size() - 1)]
-	label.text = text
+	label.text = possible_text[randi_range(0, possible_text.size() - 1)]
 
-func shrink() -> Tween:
-	print("shrinking")
-	var shrinkTween = create_tween()
-	shrinkTween.tween_property(self, "scale:x", 0, FLIP_TIME)
-	shrinkTween.tween_callback(grow)
-	print("current side is ", currentSide)
-	print("disabled current side")
-	print("done shrinking")
-	return shrinkTween
-
-func grow():
-	print("growing")
-	print(self.scale.x)
-	print("current side is ", currentSide)
-
-	swap_current_side()
-
-	var tween = create_tween()
-	state = growing
-	tween.tween_property(self, "scale:x", 1, FLIP_TIME)
-
-func swap_current_side():
-	print("swapping side was ", currentSide)
-	var oldSide = currentSide
-	if currentSide == back:
-		currentSide = front
-	elif currentSide == front:
-		currentSide = back
-	else:
-		print("side is null")
-
-	currentSide.visible = true
-	oldSide.visible = false
-	print("enabled " + str(currentSide))
-	print(currentSide.visible)
-
-func flip() -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "scale:x", 0, FLIP_TIME)
-	tween.chain().tween_callback(grow)
-	state = stay
-
-
-func test():
-	var tween = create_tween()
-	tween.tween_property(self, "scale:x", 0, FLIP_TIME)
-	tween.chain().tween_property(self, "scale:x", 1, FLIP_TIME)
-	pass
+func move(location : Vector2):
+	global_position = lerp(global_position, location, 0.2)
+	global_rotation = lerp(global_rotation, 0, 0.2)
--- Scenes/Creation Scene/creation_scene.gd
+++ Scenes/Creation Scene/creation_scene.gd
@@ -8,7 +8,10 @@ const PitchCardData = preload("res://Card/Pitch/pitch_card_data.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().create_timer(GameManager.get_round_time())
-	pass # Replace with function body.
+	
+	
+	await get_tree().create_timer(2).timeout
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
