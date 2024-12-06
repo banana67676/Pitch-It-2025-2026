90a145b05551a7e56ba95a0cf2c5f3e6a6ccae74
Teagan Thomes <teagan@anydesk.net>
2024-12-06 08:57:49 -0600

    changed import vote permission level, fixed voting button events and disabling other buttons

--- Scenes/Voting Scene/voting_scene.gd
+++ Scenes/Voting Scene/voting_scene.gd
@@ -16,26 +16,28 @@ func prepare(cards):
 	var index = 0
 	for card in cards:
 		var disp = Label.new()
-		disp.text = str(card.title," ",card.slogan, " ",card.username)
-		disp.global_position = Vector2(50,index*150+150)
+		disp.text = str(card.title, " ", card.slogan, " ", card.username)
+		disp.global_position = Vector2(50, index * 150 + 150)
 		var button = Button.new()
-		button.toggle_mode=true
+		button.toggle_mode = true
 		button.text = "Invest $100,000"
-		button.global_position = Vector2(250,index*150+150)
-		button.connect("on_pressed", lock.bind(card.user_id))
+		button.global_position = Vector2(250, index * 150 + 150)
+		button.connect("pressed", lock.bind(card.user_id))
 		disp.add_child(button)
 		$List.add_child(disp)
-		index+=1
+		index += 1
 
 func lock(user_id):
 	selection = user_id
-	for button in $List.get_children():
-		button.disable()
+	for label in $List.get_children():
+		for child in label.get_children():
+			if child is Button:
+				child.disabled = true
 
 @rpc("any_peer", "call_local", "reliable")
 func send_vote():
 	import_vote.rpc(selection)
 
-@rpc("authority", "reliable")
-func import_vote(vote : int):
+@rpc("any_peer", "reliable")
+func import_vote(vote: int):
 	MultiplayerManager.votes[multiplayer.get_remote_sender_id()] = vote
